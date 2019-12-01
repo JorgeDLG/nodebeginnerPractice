5be92b816ce948ee5fd365ee42749929b8b949f5
JorgeDLG <idroj07@gmail.com>
2019-12-01 12:25:04 +0100

    cambios menores

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require("http");
 
 
 function onRequest(request, response) {
+	console.log('petición recibida!');
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write("Hola Mundo");
 	response.end();
@@ -9,4 +10,5 @@ function onRequest(request, response) {
 
 http.createServer(onRequest).listen(8888);
 
+console.log('servidor iniciado!');
 
