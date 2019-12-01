5c9b442c97246dae782021e2f6bb22794d185836
JorgeDLG <idroj07@gmail.com>
2019-12-01 12:27:49 +0100

    exportando modulo iniciar servidor

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var http = require("http");
 
-
-function onRequest(request, response) {
+function iniciar(){
+	function onRequest(request, response) {
 	console.log('petición recibida!');
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write("Hola Mundo");
 	response.end();
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log('servidor iniciado!');
 }
-
-http.createServer(onRequest).listen(8888);
-
-console.log('servidor iniciado!');
-
+exports.iniciar = iniciar;
